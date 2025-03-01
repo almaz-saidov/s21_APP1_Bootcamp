cdf6a3ce3795f0ec95846637b2058529893ea3b2
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-03-01 22:06:46 +0300

    delete main.py

--- AP1_Py_T01-1/src/exercise1/main.py
+++
@@ -1,8 +0,0 @@
-def get_scalar_composition(v1: list[float], v2: list[float]) -> float:
-    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
-
-
-if __name__ == '__main__':
-    vector1 = list(map(float, input().split()))
-    vector2 = list(map(float, input().split()))
-    print(get_scalar_composition(vector1, vector2))
