cfc24af022c6b7837057985d9d4113759358dc0e
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-03-01 19:18:46 +0300

    2nd task: implemented a palindrome number check

--- AP1_Py_T01-1/src/exercise2/main.py
+++ AP1_Py_T01-1/src/exercise2/main.py
@@ -1,12 +1,20 @@
 def is_number_palindrome(num: int) -> bool:
     if num < 0:
         return False
+    
+    digits = []
 
+    while num > 0:
+        digits.append(num % 10)
+        num //= 10
 
-def main() -> None:
-    number = int(input())
-    print(is_number_palindrome(number))
+    for i in range(len(digits) // 2):
+        if digits[i] != digits[len(digits) - i - 1]:
+            return False
+    
+    return True
 
 
 if __name__ == '__main__':
-    main()
+    number = int(input())
+    print(is_number_palindrome(number))
